b690b56fb2b0771730196014bbd03cc54e12ee2e
Alessandro Rodriguez <alerr0406@gmail.com>
2025-09-21 17:03:16 -0500

    Refactor application structure and add dependencies for MongoDB and Swagger integration. Removed unused AppController and AppService files. Updated package.json and package-lock.json to include @nestjs/config, @nestjs/mongoose, @nestjs/swagger, class-transformer, class-validator, and mongoose. Enhanced main.ts for API documentation and validation setup.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: `mongodb://${encodeURIComponent(configService.get('MONGODB_USER') || '')}:${encodeURIComponent(configService.get('MONGODB_PASSWORD') || '')}@${configService.get('MONGODB_HOST') || 'localhost'}:${configService.get('MONGODB_PORT') || '27017'}/${configService.get('MONGODB_DATABASE') || 'test'}?authSource=admin`,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+  const apiPrefix = process.env.API_PREFIX || 'api/v1';
+  app.setGlobalPrefix(apiPrefix);
+  app.useGlobalPipes(new ValidationPipe());
+  const openapiconfig = new DocumentBuilder()
+    .setTitle('QuizifyAPI')
+    .setDescription(
+      'Aplicación Web que usa un LLM para la Creación de Ejercicios Interactivos ',
+    )
+    .setVersion('0.0.1')
+    .build();
+  const documentFactory = SwaggerModule.createDocument(app, openapiconfig);
+  SwaggerModule.setup(`${apiPrefix}/docs`, app, documentFactory);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
